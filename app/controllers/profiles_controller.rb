62734ddc91361b780dd0649685dfe09b6e33b89b
Tolu Oyeniyi <tjoye20@gmail.com>
2019-05-12 14:02:44 -0400

    Fix Profile#new form and add #images to params.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,7 +16,7 @@ class ProfilesController < ApplicationController
     if profile.save
       redirect_to new_profile_questions_path(profile.uuid), notice: 'Profile successfully created.'
     else
-      flash[:alert] = profile.error
+      flash[:alert] = profile.errors.full_messages.join(', ')
       redirect_back(fallback_location: root_path)
     end 
   end 
@@ -25,7 +25,7 @@ class ProfilesController < ApplicationController
     if @profile.update(profile_params)
       redirect_to root_path, notice: 'Profile successfully updated.'
     else
-      flash[:alert] = @profile.error
+      flash[:alert] = @profile.errors.full_messages.join(', ')
       redirect_back(fallback_location: root_path)
     end 
   end 
@@ -39,7 +39,7 @@ class ProfilesController < ApplicationController
   def profile_params
     params.require(:profile).permit(
       :gender, :gender_seeking, :bio, 
-      :race, :location
+      :race, :location, images: []
     )
   end 
 end
