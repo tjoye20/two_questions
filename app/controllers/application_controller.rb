28e4f01ebcdc5cab627ad3238b201e613d08c26b
Tolu Oyeniyi <tjoye20@gmail.com>
2019-05-13 23:24:59 -0400

    Update config to only create question route once. Add #check_if_profile_and_questions_exist to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,14 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def check_if_profile_and_questions_exist
+    if !current_user.profile 
+      redirect_to new_user_profile_path(current_user.uuid), alert: 'Please create your profile.'
+    elsif current_user.profile.questions.empty?
+      redirect_to new_profile_questions_path(current_user.profile.uuid), alert: 'Please create your profile questions.'
+    end
+  end
+
   def authenticate_user
     unless current_user
       redirect_to sign_in_path, alert: "Please sign in."
@@ -14,6 +22,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by_uuid(session[:user_uuid])
+    @current_user ||= User.includes(profile: :questions).find_by_uuid(session[:user_uuid])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,11 @@ Rails.application.routes.draw do
   get '/auth/google_oauth2/callback', to: 'users#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
   get '/profiles/:uuid/questions/new', to: 'questions#new', as: :new_profile_questions
-  post '/profiles/:uuid/questions', to: 'questions#create'
-  delete '/profiles/:uuid/questions/:id', to: 'questions#destroy', as: :delete_profile_question
   resources :views, only: :create
 
   resources :sessions, only: [:new, :destroy]
   resources :profiles, only: [:index, :show] do 
-    resources :questions, only: [:create, :destroy]
+    resources :questions, only: [:create]
   end 
   
   resources :users, only: [:create] do 
