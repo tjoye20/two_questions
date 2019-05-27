e07aba12d64edcf2a99c54ba9d4602c86628fb80
Tolu Oyeniyi <tjoye20@gmail.com>
2019-05-26 20:11:59 -0400

    Add Views relation to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :requests, dependent: :destroy
+  has_many :views, dependent: :destroy
 
   validate :create_uuid, on: :create
   validates_presence_of :email, :uuid, :display_name
