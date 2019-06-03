faf35eaf068316ad115847ff24357dc37e1912d3
Tolu Oyeniyi <tjoye20@gmail.com>
2019-06-02 23:39:26 -0400

    Move response creation to Interactor.'

--- app/interactors/requests/create_response.rb
+++ app/interactors/requests/create_response.rb
@@ -5,13 +5,22 @@ class Requests::CreateResponse
     if context.response_params.blank? || context.user_id.blank?
       context.fail!(error: "Bad context in Requests::CreateResponse. Context: #{context}")
     end 
+
+    check_if_response_exists
   end 
 
   def call
     begin 
-      Response.create_question_responses(context.response_params.merge({user_id: context.user_id}))
+      Response.create!(user_id: context.user_id, question_id: context.response_params[:question1_id], body: context.response_params[:response1])
+      Response.create!(user_id: context.user_id, question_id: context.response_params[:question2_id], body: context.response_params[:response2])
     rescue ActiveRecord::RecordInvalid => e 
       context.fail!(error: e.message)
+    end 
+  end 
+
+  def check_if_response_exists
+    unless Response.where(user_id: context.user_id, question_id: [context.response_params[:question1_id], context.response_params[:question2_id]]).empty?
+      context.fail!(error: 'You already submitted responses for this profile.')
     end 
   end 
 end 
