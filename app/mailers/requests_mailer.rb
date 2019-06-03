4b6aeb235cf2ba3c060c91e855a982eede239623
Tolu Oyeniyi <tjoye20@gmail.com>
2019-06-03 01:22:46 -0400

    Add RequestsMailer.

---
+++ app/mailers/requests_mailer.rb
@@ -0,0 +1,10 @@
+class RequestsMailer < ApplicationMailer
+
+  def notification
+    @requester      = User.find(params[:user_id])
+    @profile_owner  = Profile.find(params[:profile_id]).user
+    @response_url   = '/requests'
+
+    mail(to: @profile_owner.email, subject: "Two Questions: #{@requester.display_name.split(' ').first} responded to your profile!")
+  end 
+end
