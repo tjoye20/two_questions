96acb318fa18fee4076e538e0d712295edf81af2
Tolu Oyeniyi <tjoye20@gmail.com>
2019-06-07 19:49:24 -0400

    Add Conversations to nav. Add Conversation#index view and update Conversation#show query.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,9 +2,13 @@ class ConversationsController < ApplicationController
 
   def index
     @conversations = current_user.conversations
+
+    if @conversations.empty?
+      redirect_to root_path, notice: 'You do not have any conversations right now.'
+    end
   end 
 
   def show
-    @conversation = Conversation.includes(:messages).find_by_uuid(params[:id])
+    @conversation = Conversation.includes(:messages).where(sender_id: current_user.id).or(Conversation.includes(:messages).where(recipient_id: current_user.id)).find_by_uuid(params[:id])
   end
 end
