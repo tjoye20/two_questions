a92a5096aed8341aa5f655219f84574535de24b3
Tolu Oyeniyi <tjoye20@gmail.com>
2019-06-27 21:46:23 -0500

    Update mailer links. Update profile buttons to not sit on pictures.

--- app/mailers/approved_view_mailer.rb
+++ app/mailers/approved_view_mailer.rb
@@ -3,7 +3,7 @@ class ApprovedViewMailer < ApplicationMailer
   def notification
     @profile  = Profile.includes(:user).find(params[:profile_id])
     @user     = User.includes(:profile).find(params[:user_id])
-    @profile_url = Rails.application.routes.url_helpers.profile_url(@user.profile.uuid)}
+    @profile_url = 'https://www.asktwoquestions.co/profiles/' + @user.profile.uuid
 
     mail(to: @profile.user.email, subject: "Two Questions: #{@user.display_name.split(' ').first} liked your profile!")
   end 
--- app/mailers/requests_mailer.rb
+++ app/mailers/requests_mailer.rb
@@ -3,7 +3,7 @@ class RequestsMailer < ApplicationMailer
   def notification
     @requester      = User.find(params[:user_id])
     @profile_owner  = Profile.find(params[:profile_id]).user
-    @response_url   = Rails.application.routes.url_helpers.request_url(@user.uuid)
+    @response_url   = 'https://www.asktwoquestions.co/requests/' + @requester.uuid)
 
     mail(to: @profile_owner.email, subject: "Two Questions: #{@requester.display_name.split(' ').first} responded to your profile!")
   end 
