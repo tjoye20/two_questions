882a236bb23328742d3495125863edab9ea454a3
Tolu Oyeniyi <tjoye20@gmail.com>
2019-07-07 02:01:40 -0400

    Fix query for Interests#index.

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,7 +1,7 @@
 class InterestsController < ApplicationController
 
   def index
-    @profiles = Profile.cached_users_and_views.where(views: { user_id: current_user.id, state: 'approved'  })
+    @profiles = Profile.cached_users_views_and_requests.where(views: { user_id: current_user.id, state: 'approved'  })
     
     if @profiles.empty?
       redirect_to root_path, notice: 'You do not have any interests right now.'
